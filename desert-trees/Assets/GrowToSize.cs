86aafc80a901993c3ae9d89c1c98d827d3df20a2
Justin O'Malley <justin.t.omalley@gmail.com>
2019-12-10 20:51:23 -0500

    Adds spawning mugs

---
+++ desert-trees/Assets/GrowToSize.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GrowToSize : MonoBehaviour
+{
+    private float scale = 0;
+    
+    // Start is called before the first frame update
+    void Start() {
+        transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        while (scale < 1)
+        {
+            Debug.Log("yo");
+            transform.localScale = new Vector3(scale, scale, scale);
+            scale += Time.deltaTime;
+        }
+    }
+}
---
+++ desert-trees/Assets/MugSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MugSpawner : MonoBehaviour {
+
+    public GameObject mug;
+
+    // Next update in second
+    // Next update in second
+    private int nextUpdate = 1;
+     
+    // Update is called once per frame
+    void Update(){
+     
+        // If the next update is reached
+        if (Time.time >= nextUpdate) {
+            Debug.Log(Time.time+">="+nextUpdate);
+            nextUpdate = Mathf.FloorToInt(Time.time) + 1;
+            Instantiate(mug, transform);
+        }
+     
+    }
+}
